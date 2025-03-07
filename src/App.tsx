34b164d717f6305c46e90352f440fd2008a93dbc
kiwizu3 <kavindasu@gmail.com>
2025-03-07 22:55:08 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <img src="assets/react.svg" className="logo react" alt="React logo" />
+    </>
+  )
+}
+
+export default App
